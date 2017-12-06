1d2e3f3ab8aa08c6a78cae864c0dcccccee9910d
Roland Hauser <sourcepond@gmail.com>
2017-12-06 17:11:06 +0100

    Organized import

--- fssync-distributor-hazelcast/src/test/java/ch/sourcepond/io/fssync/distributor/hazelcast/lock/LocksTest.java
+++ fssync-distributor-hazelcast/src/test/java/ch/sourcepond/io/fssync/distributor/hazelcast/lock/LocksTest.java
@@ -35,7 +35,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
